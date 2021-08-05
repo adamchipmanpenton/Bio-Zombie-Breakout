787d38082ce95809163466dc2470eb27d2aa4e08
adamchipmanpenton <adamchipmanpenton@gmail.com>
2021-08-05 16:15:54 -0230

    Fith commit

--- Bio Zombie Breakout.js
+++ Bio Zombie Breakout.js
@@ -10,12 +10,17 @@ var doorNumber = false;
 var distanceCounter = 0;
 var doorNumberCounter = 1;
 var numberBank = [];
+let minutes2 =2;
 let minutes = 0;
 let seconds = 0;
+let milliseconds = 0;
+let myVar = null;
 
 
 const $ = selector => document.querySelector(selector);
 const padSingleDigit = num => num.toString().padStart(2, "0");
+
+
 
 function welcome(){
 
@@ -29,21 +34,46 @@ function welcome(){
 	$("#start_game").addEventListener("click", startGame);
 }
 
+
 function playAgain(){
     distanceCounter = 0;
     doorNumberCounter = 1;
     console.log("play again test")
     $("#playAgain").addEventListener("click", startGame)
 }
+
+function youWin(){
+    console.log("you win")
+    document.getElementById("textArea").value ="You were able to answer all the questions and escape to safety. I just hope you don't turn in to one later."
+    hideAnswers()
+    clearInterval(myVar)
+    document.getElementById('next_question').style.display = "none";
+    document.getElementById('playAgain').style.display = "";
+    $("#playAgain").removeEventListener("click", playAgain())
+
+    
+}
+
+let questionNumber = -1;
 function displayQuestion(){
+
+    
     if (rightWrong == false){
         distanceCounter += 1;
+        
         displayZombieDistance()
     }
+    
     if(doorNumber == true){
         doorNumberCounter += 1;
-        changeDoorNumber()
+        if (doorNumberCounter == 3){
+            youWin()
+        }else{
+            changeDoorNumber()
+        }
     }
+   
+
     document.getElementById('showAnswer').style.display = "none"; 
     document.getElementById('next_question').style.display = "none";
     question.style.display = "";
@@ -52,8 +82,16 @@ function displayQuestion(){
     answer3.style.display = ""; 
     answer4.style.display = "";
     
+    if( questionNumber != -1) {
+        $(triviaQuestions[questionNumber][5]).removeEventListener("click", correctAnswer)
+        $(triviaQuestions[questionNumber][6]).removeEventListener("click", wrongAnswer)
+        $(triviaQuestions[questionNumber][7]).removeEventListener("click", wrongAnswer)
+        $(triviaQuestions[questionNumber][8]).removeEventListener("click", wrongAnswer)
+    }
+
+
     let qNUm = Math.floor(Math.random() * numberBank.length)
-    let questionNumber = numberBank[qNUm]
+    questionNumber = numberBank[qNUm]
     numberBank.splice(qNUm, 1)
     console.log(numberBank)
     console.log(qNUm)
@@ -63,12 +101,15 @@ function displayQuestion(){
     answer3.value = triviaQuestions[questionNumber][3];
     answer4.value = triviaQuestions[questionNumber][4]; 
 
+    
 
     $(triviaQuestions[questionNumber][5]).addEventListener("click", correctAnswer)
     $(triviaQuestions[questionNumber][6]).addEventListener("click", wrongAnswer)
     $(triviaQuestions[questionNumber][7]).addEventListener("click", wrongAnswer)
     $(triviaQuestions[questionNumber][8]).addEventListener("click", wrongAnswer)
+
 }
+
 
 function wrongAnswer(){
     rightWrong = false;
@@ -80,6 +121,7 @@ function wrongAnswer(){
     document.getElementById('next_question').style.display = "";   
 }
 
+
 function correctAnswer(){
 
     rightWrong = true;
@@ -89,6 +131,13 @@ function correctAnswer(){
     document.getElementById("showAnswer").innerHTML ="You got it correct. The door opens.";
     document.getElementById('next_question').style.display = "";  
     $("#next_question").addEventListener("click", displayQuestion);   
+}
+
+function changeDoorNumber(){
+    doorNumberDisplay.innerHTML = "Door Number " + doorNumberCounter;
+    if(doorNumberCounter == 10){
+        doorNumberDisplay.innerHTML = "Exit.";
+    }
 }
 
 function makeNumberBank(){
@@ -107,64 +156,63 @@ function startGame(){
     distanceCounter = 1;
     doorNumberCounter = 1
     displayZombieDistance();
-    setInterval(countDown, 1000)
+    myVar = setInterval(countDown, 10)
 }
 
-function changeDoorNumber(){
-    doorNumberDisplay.innerHTML = "Door Number " + doorNumberCounter;
-    if(doorNumberCounter == 10){
-        doorNumberDisplay.innerHTML = "Exit.";
-    }else if(doorNumberCounter == 11){
-        document.getElementById("text_area").value ="You were able to answer all the questions and escape to safety. I just hope you don't turn in to one later."
+
+function displayZombieDistance (){
+    if (distanceCounter == 0){
+        document.getElementById("textArea").value = "Someone in the research laboratory you are working in spilt a vial of chemicals. People have "+
+        "started to turn into zombies and the laboratory was placed in lockdown. There are ten doors between you and the outside. You must enter the " +
+        "correct password for each door to escape. You must enter them correctly or the zombies will catch you.";
+        document.getElementById('health').src = "images/Health 1.jpg";
+        
+    }
+    else if(distanceCounter == 1){
+	    document.getElementById("textArea").value = "The only noise you hear is the alarms. You must move quickly before the zombies find you."
+        document.getElementById('health').src = "images/Health 2.jpg";
+        
+        
+    }else if(distanceCounter == 2){
+	    document.getElementById("textArea").value = "It is too late, you start to hear noise in the distance. Something is approaching."
+        document.getElementById('health').src = "images/Health 3.jpg";
+        
+         
+    }else if(distanceCounter == 3){
+	    document.getElementById("textArea").value = "You see a large group of zombies starting to come down the hallway towards you."
+        document.getElementById('health').src = "images/Health 4.jpg"; 
+       
+    }else if(distanceCounter == 4){
+        document.getElementById('health').src = "images/Health 5.jpg"; 
+        document.getElementById("textArea").value ="You could not get out in time. A group of zombies have surrounded you and you are bitten. You slowly turn into a zombie."
         hideAnswers()
         document.getElementById('next_question').style.display = "none";
         document.getElementById('playAgain').style.display = "";
         playAgain()
+        
     }
-}
 
-function displayZombieDistance (){
-    if (distanceCounter == 0){
-        document.getElementById("text_area").value = "Someone in the research laboratory you are working in spilt a vial of chemicals. People have "+
-        "started to turn into zombies and the laboratory was placed in lockdown. There are ten doors between you and the outside. You must enter the " +
-        "correct password for each door to escape. You must enter them correctly or the zombies will catch you. You have two minutes to escape.";
-        document.getElementById('health').src = "images/Health 1.jpg";  
-    }else if(distanceCounter == 1){
-	    document.getElementById("text_area").value = "The only noise you hear is the alarms. You must move quickly before the zombies find you."
-        document.getElementById('health').src = "images/Health 2.jpg";  
-    }else if(distanceCounter == 2){
-	    document.getElementById("text_area").value = "It is too late, you start to hear noise in the distance. Something is approaching."
-        document.getElementById('health').src = "images/Health 3.jpg"; 
-    }else if(distanceCounter == 3){
-	    document.getElementById("text_area").value = "You see a large group of zombies starting to come down the hallway towards you."
-        document.getElementById('health').src = "images/Health 4.jpg";  
-    }else if(distanceCounter == 4){
-        document.getElementById('health').src = "images/Health 5.jpg"; 
-        document.getElementById("text_area").value ="You could not get out in time. A group of zombies have surrounded you and you are bitten. You slowly turn into a zombie."
-        hideAnswers()
-        document.getElementById('next_question').style.display = "none";
-        document.getElementById('playAgain').style.display = "";
-        playAgain()  
-    }
 };
 
 function countDown(){
-    seconds += 1;
+
+    milliseconds += 10;
+    if(milliseconds == 1000){
+        seconds += 1;
+        milliseconds = 0;
+    }    
     if (seconds == 60){
         seconds = 0;
         minutes += 1;
-    }
-    if (minutes == 2){
-        if(seconds == 0){
-            document.getElementById("text_area").value = "You ran out of time!!"
-            hideAnswers()
-            document.getElementById('next_question').style.display = "none";
-            document.getElementById('playAgain').style.display = "";
-            document.getElementById('timer').style.display = "none";
-            document.getElementById('health').src = "images/Health 5.jpg"; 
-            console.log("times up")
-            playAgain()
-        }
+    }if (minutes == 2){
+        document.getElementById("textArea").value = "You ran out of time!!"
+        hideAnswers()
+        document.getElementById('next_question').style.display = "none";
+        document.getElementById('playAgain').style.display = "";
+        document.getElementById('timer').style.display = "none";
+        document.getElementById('health').src = "images/Health 5.jpg"; 
+        console.log("times up")
+        playAgain()
     }
     updateCountDown(minutes, seconds)
 }
@@ -172,6 +220,7 @@ function countDown(){
 const  updateCountDown = (minutes, seconds) => {
     document.getElementById("minutes").textContent = padSingleDigit(minutes);
     document.getElementById("seconds").textContent = padSingleDigit(seconds);
+    document.getElementById("milliseconds").textContent = padSingleDigit(milliseconds);
 }
 
 function hideAnswers(){
@@ -189,7 +238,14 @@ document.addEventListener("DOMContentLoaded", () => {
     hideAnswers()
     displayZombieDistance()
 	$("#start_game").addEventListener("click", startGame);
+
+
 });
+const answerKeyA = ["#answer1", "#answer2", "#answer3", "#answer4"]
+const answerKeyB = ["#answer2", "#answer3", "#answer4", "#answer1"]
+const answerKeyC = ["#answer3", "#answer4", "#answer1", "#answer2"]
+const answerKeyD = ["#answer4", "#answer3", "#answer2", "#answer1"]
+
 
 const triviaQuestions = [
     ["All of the following are parts of the immune system except", "spleen", "lymph nodes", "pancreas", "leucocytes", "#answer3", "#answer4", "#answer1", "#answer2" ],
@@ -212,3 +268,9 @@ const triviaQuestions = [
     ["Ms. Courtney's brother has an allergic reaction after taking a bite of stuffed chicken breast.  Turns out the pesto contains walnuts.  What should they do next?", "Tell him to walk it off, he'll be ok",  "Give him a tissue, as he'll probably have watery eyes as an allergic response", 'Tell him to go to the next room, away from the pesto', 'Get the guy an epi-pen, he has an anaphylactic allergy to nuts', "#answer4", "#answer3", "#answer2", "#answer1"],
     ["Which of the following has required an 'iron lung' as a form of treatment in the past?", "Measles", "HPV", "Polio", "Whooping Cough", "#answer3", "#answer4", "#answer1", "#answer2"]
 ];
+
+
+
+
+
+//document.getElementById("answer1").removeEventListener("click")
